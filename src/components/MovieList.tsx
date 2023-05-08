c0253529f4fe5357012f6f2df9faeacbeb6d5ec7
Olivia Webster <oliviacweb@gmail.com>
2023-05-08 01:18:11 -0600

    Add select and function to filter movies by genre

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -9,7 +9,7 @@ export interface Movie {
 }
 
 export default function MovieList() {
-    const [movies, setMovies] = useState([] as Movie[]);
+    const [movies, setMovies] = useState<Movie[]>([]);
     useEffect(() => {
         const fetchData = async () => {
             const movies = await getMovies();
@@ -19,6 +19,7 @@ export default function MovieList() {
         fetchData();
     }, []);
 
+    //Get image by ID
     function getImageUrl(id: string): string {
         try {
             return require(`../images/${id}.jpeg`);
@@ -27,11 +28,26 @@ export default function MovieList() {
         }
     }
 
+    // Filter movies by either 'All' or specific genre
+    const [genre, setGenre] = useState<string>('All');
+    const filteredMovies = genre === 'All' ? movies : movies.filter(movie => movie.genres.includes(genre));
+    const genres = Array.from(
+        new Set(movies.flatMap(movie => movie.genres))
+    );
+
     return (
         <div className="container">
-            <h1 className="title">Movie List</h1>
+            <div className="menu">
+                <h1 className="title">Movie List</h1>
+                <select className="dropdown" value={genre} onChange={e => setGenre(e.target.value)}>
+                    <option value="All">All</option>
+                    {genres.map(genre => (
+                        <option key={genre} value={genre}>{genre}</option>
+                    ))}
+                </select>
+            </div>
             <div className="grid">
-                {movies.map(movie => (
+                {filteredMovies.map(movie => (
                     <div key={movie.id} className="card">
                         <img src={getImageUrl(movie.id)} alt={movie.title} className="card__image" />
                         <p className="card__title" >{movie.title}</p>
