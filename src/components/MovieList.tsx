1b5f344259f57266c4490b5b1eafaedef9e97237
Olivia Webster <oliviacweb@gmail.com>
2023-05-08 00:27:56 -0600

    map through and display initital move data

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getMovies } from '../services/movieService';
+
+export interface Movie {
+    id: string;
+    title: string;
+    genres: string[];
+}
 
 export default function MovieList() {
+    const [movies, setMovies] = useState([] as Movie[]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const movies = await getMovies();
+            console.log(movies)
+            setMovies(movies);
+        };
+        fetchData();
+    }, []);
     return (
-        <div>Hello from MovieList</div>
+        <div>
+            <h1>Movie List</h1>
+            {movies.map(movie => (
+                <div key={movie.id}>
+                    <h2>{movie.title}</h2>
+                    <ul>
+                        {movie.genres.map(genre => (
+                            <li key={genre}>{genre}</li>
+                        ))}
+                    </ul>
+                </div>
+            ))
+            }
+        </div>
     );
 }
