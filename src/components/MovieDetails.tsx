b967a17d12620b72695bbf3f9fa1d4c485f1f8e3
Olivia Webster <oliviacweb@gmail.com>
2023-05-08 02:32:45 -0600

    fetch the movie detail in useEffect

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { getMovieDetails } from "../services/movieService";
 
 type Props = {};
+
+interface MovieDetails {
+    id: string;
+    title: string;
+    genres: string[];
+    duration: number;
+    releaseDate: string;
+    releaseYear: number;
+    topCast: string[];
+    moods: string[];
+}
 
 export default function MovieDetails({ }: Props) {
     // Get movie ID from React Router params
     const { id } = useParams();
 
+    // Fetch movie details from API
+    const [movie, setMovie] = useState<MovieDetails>({} as MovieDetails);
+    useEffect(() => {
+        const fetchData = async () => {
+            const movie = await getMovieDetails(id || '');
+            setMovie(movie);
+            console.log(movie)
+        };
+        fetchData();
+    }, []);
+
     return (
         <div>
             <h2>
-                Movie ID: {id}
+                Movie ID: {movie.id}
             </h2>
             <button>
                 <Link to="/">Go Back</Link>
